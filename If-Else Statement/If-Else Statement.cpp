e5cd16c4775f570769074b81fef8440f5cb5cd39
peterkk2 <blissfulkaaya@gmail.com>
2024-09-22 12:37:20 +0300

    Add files via upload

---
+++ If-Else Statement/If-Else Statement.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  int age = 18;
+  if (age >= 18) {
+    std::cout << "You are eligible to vote." << std::endl;
+  } else {
+    std::cout << "You are not eligible to vote." << std::endl;
+  }
+
+  return 0;
+}
